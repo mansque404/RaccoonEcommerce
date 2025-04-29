98d0cdf3727339467c85082fd8640cae3c555f77
Vitor Mansque <mansquevitor@gmail.com>
2025-04-29 00:17:49 -0300

    first commit raccoonEcommerce

---
+++ RaccoonEcommerceAPI/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles(); 
+
+app.MapControllers();
+app.Run();
